7748c2b698f1392dea855c4aed762b1aebf6bd5b
Andre Lima <andresdslima@gmail.com>
2025-08-21 09:49:17 +0100

    Initial Commit

---
+++ myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by Andre Lima on 21/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
